d06c78cc0c92e21fbfadb13400de8de15bb7f839
viek4nag <vivek4nag@gmail.com>
2025-01-17 23:10:16 +0530

    small edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@
 const Header = () => {
   return (
     <div className="bg-zinc-900 w-full h-12 p-2 flex flex-row ">
+      
         <h2 className="text-lg font-semibold text-violet-500 pl-8">Trello</h2>
     </div>
   )
